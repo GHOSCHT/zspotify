547c2b1393c2a27dc003067d94af8dcd35359430
GHOSCHT <31184695+GHOSCHT@users.noreply.github.com>
2022-10-14 20:30:12 +0200

    Add zspotify + loudgain script

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "Please supply a song/playlist url as an argument"
+    exit 1
+fi
+
+poetry run python zspotify $1
+loudgain -I4 -S -r -k -s e ./Spotify/rec/*/*.m4a
+loudgain -I4 -S -r -k -s e ./Spotify/rec/*/*.mp3
